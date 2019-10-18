30dce78a407eaffdd9ee53a45c0d5d2fd8505167
ASweilam <anhsbio@gmail.com>
2019-10-18 20:44:20 +0100

    Added js and index.js file with some test code.

---
+++ wwwroot/js/index.js
@@ -0,0 +1,9 @@
+﻿
+var theForm = document.getElementById("theForm");
+
+theForm.hidden = true;
+var button = document.getElementById("BuyButton");
+button.addEventListener("click", function () {
+    console.log("Buying Item");
+});
+
