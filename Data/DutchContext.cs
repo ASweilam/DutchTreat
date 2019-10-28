0cb8f00bbb1154a98ac8d4160ebe635df686d650
ASweilam <anhsbio@gmail.com>
2019-10-28 18:32:56 +0000

    Added dbcontext and override method for onmodelcreating.

--- Data/DutchContext.cs
+++ Data/DutchContext.cs
@@ -1,15 +1,36 @@
 ﻿using DutchTreat.Data.Entities;
+using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DutchTreat.Data
 {
     public class DutchContext:DbContext
     {
+        public DutchContext(DbContextOptions<DutchContext> options) : base(options)
+        {
+        }
+
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Order>().HasData(new Order() { 
+            Id = 1,
+            OrderDate = DateTime.Now,
+            OrderNumber = "12345"
+            });
+
+        }
     }
 }
