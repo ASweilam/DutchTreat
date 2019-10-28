adf1dc118b50507256452f1f36776c0908edfee6
ASweilam <anhsbio@gmail.com>
2019-10-28 18:32:32 +0000

    Added seeder file to seed initial data. This is not complete yet!

---
+++ Data/DutchSeeder.cs
@@ -0,0 +1,42 @@
+﻿using DutchTreat.Data.Entities;
+using Microsoft.AspNetCore.Hosting;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Data
+{
+    public class DutchSeeder
+    {
+        private readonly DutchContext _ctx;
+        private readonly IHostingEnvironment _hosting;
+
+        public DutchSeeder(DutchContext ctx, IHostingEnvironment hosting)
+        {
+            _ctx = ctx;
+            hosting = _hosting;
+        }
+
+        public void Seed()
+        {
+            //esure the db exists
+            _ctx.Database.EnsureCreated();
+            if (!_ctx.Products.Any())
+            {
+                var filepath = Path.Combine(_hosting.ContentRootPath, "Data/art.json");
+                var json = File.ReadAllText(filepath);
+                var products = JsonConvert.DeserializeObject<IEnumerable<Product>>(json);
+                _ctx.Products.AddRange(products);
+
+                var order = _ctx.Orders.Where(o => o.Id == 1).FirstOrDefault();
+
+
+                _ctx.SaveChanges();
+            }
+            
+        }
+    }
+}
