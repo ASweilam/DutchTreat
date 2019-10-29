489d4101e7f7d32c38409e4c9ccb2a6c13c79d80
ASweilam <anhsbio@gmail.com>
2019-10-29 17:54:31 +0000

    added 2nd migration

---
+++ Migrations/20191029175108_SecondSeed.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DutchTreat.Migrations
+{
+    public partial class SecondSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2019, 10, 29, 17, 51, 8, 97, DateTimeKind.Local).AddTicks(5626));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2019, 10, 29, 17, 12, 17, 35, DateTimeKind.Local).AddTicks(1982));
+        }
+    }
+}
