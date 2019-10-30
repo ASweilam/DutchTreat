21ce55982a2c4999764988b8697f16d1cc10afbe
ASweilam <anhsbio@gmail.com>
2019-10-30 17:57:42 +0000

    Added Orders API

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DutchTreat.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DutchTreat.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IDutchRepository _repository;
+        private readonly ILogger<OrdersController> _logger;
+
+        public OrdersController(IDutchRepository repository, ILogger<OrdersController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        public IActionResult Get()
+        {
+
+            try
+            {
+                return Ok(_repository.GetAllOrders());
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError($"Failed to get Order: {ex}");
+                return BadRequest("Failed to get orders");
+            }
+        }
+
+    }
+
+
+}
