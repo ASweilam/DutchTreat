a97b98da59ee84f2f323ed66cfebf935488eb6cd
ASweilam <anhsbio@gmail.com>
2019-11-04 19:37:43 +0000

    Added Transient seeder service and changed the default connection name.

--- Startup.cs
+++ Startup.cs
@@ -14,63 +14,60 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Newtonsoft.Json;
 
 namespace DutchTreat
 {
-  public class Startup
-  {
-    private readonly IConfiguration _config;
-
-    public Startup(IConfiguration config)
+    public class Startup
     {
-      _config = config;
-    }
+        private readonly IConfiguration _config;
 
-    // This method gets called by the runtime. Use this method to add services to the container.
-    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddDbContext<DutchContext>(cfg =>
-      {
-        cfg.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
-      });
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
 
-      services.AddTransient<DutchSeeder>();
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DutchContext>(cfg =>
+            {
+                cfg.UseSqlServer(_config.GetConnectionString("DutchConnectionString"));
+            });
 
-      services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
-      services.AddScoped<IDutchRepository,DutchRepository>();
+            services.AddTransient<DutchSeeder>();
 
+            services.AddScoped<IDutchRepository, DutchRepository>();
 
-      services.AddTransient<IMailService, NullMailService>();
+            services.AddTransient<IMailService, NullMailService>();
             // Support for real mail service
 
-            //services.AddControllersWithViews().AddJsonOptions(opt=>opt.JsonSerializerOptions);
             services.AddControllersWithViews();
 
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseNodeModules();
+
+            app.UseRouting();
+
+            app.UseEndpoints(cfg =>
+            {
+                cfg.MapControllerRoute("Default",
+                      "{controller}/{action}/{id?}",
+                      new { controller = "App", Action = "Index" });
+            });
+
+        }
     }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-
-      app.UseStaticFiles();
-      app.UseNodeModules();
-
-      app.UseRouting();
-
-      app.UseEndpoints(cfg =>
-      {
-        cfg.MapControllerRoute("Default",
-              "{controller}/{action}/{id?}",
-              new { controller = "App", Action = "Index" });
-      });
-
-    }
-  }
 }
