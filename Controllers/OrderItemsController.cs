ce1f62d0fbc3a2ae6173659aa79849a33b76b97b
ASweilam <anhsbio@gmail.com>
2019-11-05 18:10:18 +0000

    Jusr formatting.

--- Controllers/OrderItemsController.cs
+++ Controllers/OrderItemsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using DutchTreat.Data;
@@ -9,52 +10,48 @@ using DutchTreat.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace DutchTreat.Controllers
 {
-    [Route("/api/orders/{Orderid}/items")]
+    [Route("/api/orders/{orderid}/items")]
     public class OrderItemsController : Controller
     {
         private readonly IDutchRepository _repository;
         private readonly ILogger<OrderItemsController> _logger;
         private readonly IMapper _mapper;
 
-        public OrderItemsController(IDutchRepository repository, ILogger<OrderItemsController> logger
-            , IMapper mapper)
+        public OrderItemsController(IDutchRepository repository,
+          ILogger<OrderItemsController> logger,
+          IMapper mapper)
         {
             _repository = repository;
             _logger = logger;
             _mapper = mapper;
         }
 
-
-
-        // GET: /<controller>/
         [HttpGet]
         public IActionResult Get(int orderId)
         {
             var order = _repository.GetOrderById(orderId);
-            if(order != null) return Ok(_mapper.Map<IEnumerable<OrderItem>, IEnumerable<OrderItemViewModel>>(order.Items));
+            if (order != null) return Ok(_mapper.Map<IEnumerable<OrderItem>, IEnumerable<OrderItemViewModel>>(order.Items));
             return NotFound();
         }
 
         [HttpGet("{id}")]
-
         public IActionResult Get(int orderId, int id)
         {
             var order = _repository.GetOrderById(orderId);
             if (order != null)
             {
                 var item = order.Items.Where(i => i.Id == id).FirstOrDefault();
-                if(item !=null)
-                { 
+                if (item != null)
+                {
                     return Ok(_mapper.Map<OrderItem, OrderItemViewModel>(item));
                 }
-                
             }
             return NotFound();
+
         }
+
 
     }
 }
