3c55041836550f94b85d64b78138235f660a3fa3
ASweilam <anhsbio@gmail.com>
2019-11-05 18:11:22 +0000

    formatting and spacing.

--- Data/DutchMappingProfile.cs
+++ Data/DutchMappingProfile.cs
@@ -1,10 +1,11 @@
-﻿using AutoMapper;
-using DutchTreat.Data.Entities;
-using DutchTreat.ViewModels;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using DutchTreat.Data.Entities;
+using DutchTreat.ViewModels;
 
 namespace DutchTreat.Data
 {
@@ -12,11 +13,12 @@ namespace DutchTreat.Data
     {
         public DutchMappingProfile()
         {
-            CreateMap<Order, OrderViewModel>().ForMember(o=>o.OrderId, ex=>ex.MapFrom(o=>o.Id))
-                .ReverseMap();
+            CreateMap<Order, OrderViewModel>()
+              .ForMember(o => o.OrderId, ex => ex.MapFrom(o => o.Id))
+              .ReverseMap();
 
             CreateMap<OrderItem, OrderItemViewModel>()
-                .ReverseMap();
+              .ReverseMap();
         }
     }
 }
