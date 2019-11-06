a55b0e30c4e4dc93705ed5fed5107500ee96e46f
ASweilam <anhsbio@gmail.com>
2019-11-06 19:52:29 +0000

    Added storecustomer TypeScript with a constructor and a function (a method).

--- wwwroot/ts/storecustomer.ts
+++ wwwroot/ts/storecustomer.ts
@@ -1,4 +1,22 @@
 ﻿
 class StoreCustomer {
 
+    constructor(private firstName: string, private lastName:string) {
+ 
+    }
+
+    public visits = 0;
+    private ourName: string;
+
+    public showName() {
+        alert(this.firstName+ " "+ this.lastName);
+    }
+
+    set name(val){
+        this.ourName = val;
+    }
+
+    get name(){
+        return this.ourName;
+    }
 }
