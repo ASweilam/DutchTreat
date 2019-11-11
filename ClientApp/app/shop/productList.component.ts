ded300086f15c3c591417ce38f088df85431b1d2
ASweilam <anhsbio@gmail.com>
2019-11-11 19:40:21 +0000

    added product list component.

--- ClientApp/app/shop/productList.component.ts
+++ ClientApp/app/shop/productList.component.ts
@@ -17,3 +17,26 @@ export class ProductList {
     price: 14.99
   }];
 }
+
+
+//import { Component } from "@angular/core";
+
+//@Component({
+//    selector: "product-list",
+//    templateUrl: "productList.component.html",
+//    styleUrls:[]
+//})
+
+//export class ProductList {
+//    public products = [{
+//        title: "First Product",
+//        price: 19.99
+//    }, {
+//        title: "Second Product",
+//        price: 9.99
+//    }, {
+//        title: "Third Product",
+//        price: 14.99
+//    }];
+
+//}
